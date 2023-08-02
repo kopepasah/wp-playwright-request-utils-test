015ffabeed0fd25a80e917815c6bdfe4fb3d0e84
Justin Kopepasah <justin.kopepasah@lendedu.com>
2023-08-02 17:25:19 -0500

    Remove unneded code.

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { test, expect, request } from '@playwright/test';
+import { test } from '@playwright/test';
 
 /**
  * WordPress dependencies
@@ -11,26 +11,6 @@ import { RequestUtils } from '@wordpress/e2e-test-utils-playwright';
 test( 'test get post from RequestUtils on custom port', async ( { page } ) => {
 	const requestUtils = await RequestUtils.setup( {
 		baseURL: 'http://localhost:8181',
-		user: {
-			username: 'admin',
-			password: 'password',
-		},
-	} );
-
-	const posts = await requestUtils.rest( {
-		path: '/wp/v2/posts',
-		params: {
-			per_page: 1,
-			status: 'publish',
-		},
-	} );
-
-	console.log( posts );
-} );
-
-test( 'test get post from RequestUtils on custom test port', async ( { page } ) => {
-	const requestUtils = await RequestUtils.setup( {
-		baseURL: 'http://localhost:8189',
 		user: {
 			username: 'admin',
 			password: 'password',
